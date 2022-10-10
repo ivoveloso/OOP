080932b82ea03807e6d7564f5ce58d54efcb6520
ivoveloso <ivovg5790@gmail.com>
2022-10-10 23:06:40 +1100

    Added initial html generator

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown.js');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const generateHTML = require('./src/append');
 
 // TODO: Create an array of questions for user input
 const questions = [
-    `What is your GitHub username?`, //0
-    `What is your email address?`,//1
-    `What is your project's name?`,//2
-    `Please write a short description of your project:`,//3
-    `What kind of license should your project have?`,//4
-    `What command should be run to install dependencies?`,//5
-    `What command should be run to run tests?`,//6
-    `What does the user need to know about using the repo?`,//7
-    `What does the user need to know about contributing to the repo?`//8
+    `What is your team manager's name?`, //0
+    `What is his employee ID?`,//1
+    `What is his email address?`,//2
+    `What is his office number?`,//3
+    `Would you like to add a another team member?` //4
 ];
 
-const licenses = [
-    `MIT`,
-    `Mozzila`,
-    'Apache',
+const engQs = [
+  `What is your engineer's name?`, //0
+  `What is his employee ID?`,//1
+  `What is his email address?`,//2
+  `What is his GitHub?`,//3
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {    
-fs.writeFile(`./utils/${fileName}.md`,`${generateMarkdown(data)}\n`,
- (err) => err ? console.error(err) : console.log('README ready!'))
-}
+const intQs = [
+  `What is your intern's name?`, //0
+  `What is his employee ID?`,//1
+  `What is his email address?`,//2
+  `What is his school's name?`,//3
+];
+
+const roles = [
+    `Engineer`,
+    `Intern`,
+    `Finish building my team`
+];
 
 // TODO: Create a function to initialize app
 function init() {
@@ -35,51 +41,111 @@ function init() {
       {
         type: 'input',
         message: questions[0],
-        name: 'username',
+        name: 'name',
       },
       {
         type: 'input',
         message: questions[1],
-        name: 'email',
+        name: 'eID',
       },
       {
         type: 'input',
         message: questions[2],
-        name: 'title',
+        name: 'email',
       },
       {
         type: 'input',
         message: questions[3],
-        name: 'description',
+        name: 'officeNum',
       },
+    ])
+    .then(val => {
+      generateHTML(val);
+      finalQ(val);
+      })}
+
+function finalQ(data) {
+    const manager = new Manager(data.name, data.eID, data.email, data.officeNum);
+    inquirer
+    .prompt([
       {
         type: 'list',
         message: questions[4],
-        choices: licenses,
-        name: 'license',
+        choices: roles,
+        name: 'role',
+      }
+    ])
+    .then(val => {
+      if (val.role === `Engineer`){
+        engineer();
+      } else if(val.role === 'Intern') {
+        intern();
+      } else {
+        console.info('Your team is ready. HTML generated!');
+        console.info(manager.getRole());
+      }
+})
+    }
+
+function engineer() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: engQs[0],
+        name: 'name',
       },
       {
         type: 'input',
-        message: questions[5],
-        name: 'install',
+        message: engQs[1],
+        name: 'eID',
       },
       {
         type: 'input',
-        message: questions[6],
-        name: 'test',
+        message: engQs[2],
+        name: 'email',
       },
       {
         type: 'input',
-        message: questions[7],
-        name: 'usage',
-      },
-      {
-        type: 'input',
-        message: questions[8],
-        name: 'contribute',
+        message: engQs[3],
+        name: 'github',
       },
     ])
-    .then((data) => writeToFile(`README`, data));}
+    .then(val => {
+      const engineer = new Engineer(val.name, val.eID, val.email, val.github);
+      finalQ(val);
+      })
+  }
+
+function intern() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: intQs[0],
+        name: 'name',
+      },
+      {
+        type: 'input',
+        message: intQs[1],
+        name: 'eID',
+      },
+      {
+        type: 'input',
+        message: intQs[2],
+        name: 'email',
+      },
+      {
+        type: 'input',
+        message: intQs[3],
+        name: 'school',
+      },
+    ])
+    .then(val => {
+      const intern = new Intern(val.name, val.eID, val.email, val.school);
+      finalQ(val);
+    })
+}
 
 // Function call to initialize app
 init();
---
+++ src/append.js
@@ -0,0 +1,64 @@
+const fs = require('fs');
+
+
+// TODO: Create a function to append data to html file
+function generateHTML(data) {    
+fs.writeFile(`./dist/index.html`,`${basicLayout(data)}\n`,
+ (err) => err ? console.error(err) : console.log('HTML ready!'))
+}
+
+function basicLayout (data) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,400i,700&display=swap" rel="stylesheet">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+      <link rel="stylesheet" href="./style.css" />
+      <title>My Team</title>
+    </head>
+    
+    <body>
+      <header style="background-color: #c24949;">
+        <div class="container col-xxl-8 px-4 py-5">
+            <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+              <div class="col-lg">
+                <h1 class="display-5 fw-bold">My Team</h1>
+                <p class="lead">Quickly create your team members' cards!</p>
+                </div>
+              </div>
+            </div>
+          </div>
+      </header>
+    
+      <main>
+        <div class="row" id="main">
+            <div class="col-sm-3">
+              <div class="card text-center">
+                <div class="card-body align-items-center row">
+                    <div style="background-color: #2d48df; padding: 10px">
+                        <h4 class="card-title" style="color:white">${data.name}</h4>
+                        <h4 class="card-title fa fa-coffee" style="color:white"> Manager</h4>
+                    </div>
+                  <a class="card-footer text-muted">ID: ${data.eID}</a>
+                  <a href="mailto:${data.email}" class="card-footer text-muted">Email: ${data.email}</a>
+                  <a class="card-footer text-muted">Office Number: ${data.officeNum}</a>
+                </div>
+              </div>
+            </div> 
+          </div>
+      </main>
+    
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+    </body>
+    
+    </html>`
+}
+
+module.exports = generateHTML;
